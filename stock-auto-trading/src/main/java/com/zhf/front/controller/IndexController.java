662476892b7e547b775b2985b118b2e6d8b5b100
hongfei.zhang <hongfei.zhang@ly.com>
2019-04-18 00:31:13 +0800

    增加信息披露页面

--- stock-auto-trading/src/main/java/com/zhf/front/controller/IndexController.java
+++ stock-auto-trading/src/main/java/com/zhf/front/controller/IndexController.java
@@ -1,10 +1,20 @@
 package com.zhf.front.controller;
 
 import com.zhf.common.controller.BaseController;
+import com.zhf.common.domain.ResponseBo;
+import com.zhf.common.util.MD5Utils;
+import com.zhf.system.service.UserService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by hongfei.zhang on 2019/4/13
@@ -12,6 +22,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController extends BaseController {
     private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private static final String CODE_KEY = "_code";
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/front")
     public String front() {
@@ -23,6 +38,11 @@ public class IndexController extends BaseController {
         return "front/newbie";
     }
 
+    @GetMapping("/front/info")
+    public String infoOpen(){
+        return "/front/info_open";
+    }
+
     @GetMapping("/front/login")
     public String login() {
         return "front/login";
@@ -31,6 +51,27 @@ public class IndexController extends BaseController {
     @GetMapping("/front/register")
     public String register() {
         return "front/register";
+    }
+
+    @PostMapping("/front/login")
+    @ResponseBody
+    public ResponseBo login(String username, String password, Boolean rememberMe) {
+
+        // 密码 MD5 加密
+        password = MD5Utils.encrypt(username.toLowerCase(), password);
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password, rememberMe);
+        try {
+            Subject subject = getSubject();
+            if (subject != null)
+                subject.logout();
+            super.login(token);
+            this.userService.updateLoginTime(username);
+            return ResponseBo.ok();
+        } catch (UnknownAccountException | IncorrectCredentialsException | LockedAccountException e) {
+            return ResponseBo.error(e.getMessage());
+        } catch (AuthenticationException e) {
+            return ResponseBo.error("认证失败！");
+        }
     }
 
 }
--- stock-auto-trading/src/main/resources/static/js/register.js
+++ stock-auto-trading/src/main/resources/static/js/register.js
@@ -1,16 +1,16 @@
-$(function(){
+$(function () {
     //输入框获取焦点-失去焦点
-    $(".register>form input").each(function(i,elem){
-        $(elem).focus(e=>{  //绑定获取焦点事件
+    $(".register>form input").each(function (i, elem) {
+        $(elem).focus(e => {  //绑定获取焦点事件
             var $tar = $(e.target);
             $tar.parent().removeClass().addClass("show");
         });
-        $(elem).blur(e=>{  //绑定失去焦点事件
+        $(elem).blur(e => {  //绑定失去焦点事件
             var $tar = $(e.target);
-            if($tar.val()==""){
+            if ($tar.val() == "") {
                 //如果input值为空，给其父元素添加default样式，label将会在输入框中
                 $tar.parent().removeClass().addClass("default");
-            }else{
+            } else {
                 //如果input值不为空，给其父元素多添加一个blur样式，是为了让label能够在输入框上方，不会到输入框中间
                 $tar.parent().removeClass().addClass("default").addClass("blur");
             }
@@ -18,40 +18,40 @@ $(function(){
     })
 
     //给注册按钮绑定单击事件，进行表单验证
-    var nReg = new RegExp("^\\w{3,10}$","i");
+    var nReg = new RegExp("^\\w{3,10}$", "i");
     var pReg = new RegExp("^\\w{8,10}$");
     var phReg = new RegExp("^[1][3,5,7,8]\\d{9}$");
     var $uname = $("[data-form=uname]");
     var $upwd = $("[data-form=upwd]");
     var $upwd_2 = $("[data-form=upwd_2]");
     var $phone = $("[data-form=phone]");
-    $("[data-form=submit]").click(e=>{
+    $("[data-form=submit]").click(e => {
         e.preventDefault();
-        if(!nReg.test($uname.val().trim())){
+        if (!nReg.test($uname.val().trim())) {
             //验证不通过，让输入框获取焦点，并给其父元素添加error样式
             $uname.trigger("focus").parent().removeClass().addClass("error");
             //程序停止向下执行；
             return;
-        }else{
+        } else {
             //验证通过，给其父元素添加default样式，和blur样式，让label在输入框上方
             $uname.parent().removeClass().addClass("default").addClass("blur");
         }
-        if(!pReg.test($upwd.val().trim())){
+        if (!pReg.test($upwd.val().trim())) {
             $upwd.trigger("focus").parent().removeClass().addClass("error");
             return;
-        }else{
+        } else {
             $upwd.parent().removeClass().addClass("default").addClass("blur");
         }
-        if($upwd_2.val().trim()!=$upwd.val().trim()){
+        if ($upwd_2.val().trim() != $upwd.val().trim()) {
             $upwd_2.trigger("focus").parent().removeClass().addClass("error");
             return;
-        }else{
+        } else {
             $upwd_2.parent().removeClass().addClass("default").addClass("blur");
         }
-        if(!phReg.test($phone.val().trim())){
+        if (!phReg.test($phone.val().trim())) {
             $phone.trigger("focus").parent().removeClass().addClass("error");
             return;
-        }else{
+        } else {
             $phone.parent().removeClass().addClass("default").addClass("blur");
         }
         //验证通过向data/user/register.php发送ajax请求
@@ -59,14 +59,14 @@ $(function(){
             upwd = $upwd.val().trim(),
             phone = $phone.val().trim();
         $.ajax({
-            type:"POST",
-            url:"data/user/register.php",
-            data:{uname:uname,upwd:upwd,phone:phone},
-            success:function(data){
-                if(data.ok==1){
+            type: "POST",
+            url: "data/user/register.php",
+            data: {uname: uname, upwd: upwd, phone: phone},
+            success: function (data) {
+                if (data.ok == 1) {
                     alert(data.msg);
                     location.assign("login.html");
-                }else{
+                } else {
                     alert(data.msg);
                 }
             }
@@ -75,18 +75,18 @@ $(function(){
 
     //给注册按钮绑定鼠标移入移出事件
     $("[data-form=submit]").hover(
-        e=>{
+        e => {
             var $tar = $(e.target);
             $tar.toggleClass("s_hover");
         }
     )
 
     //给微信、QQ、微博图标绑定鼠标移入移出事件
-    $("[data-log=icon]").each(function(i,elem){   //遍历父元素获取其下标和元素
+    $("[data-log=icon]").each(function (i, elem) {   //遍历父元素获取其下标和元素
         //给其父元素绑定hover事件
-        var $tar=$(elem);
+        var $tar = $(elem);
         $(elem).hover(
-            e=>{
+            e => {
                 $tar.toggleClass("i_hover");
             }
         )
