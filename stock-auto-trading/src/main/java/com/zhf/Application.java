ffe6c6a2fe82b751df4d3e2bbd6e8d3865476d1c
hongfei.zhang <hongfei.zhang@ly.com>
2019-05-10 14:34:00 +0800

    增加了后端的主页背景图，修复bug

--- stock-auto-trading/src/main/java/com/zhf/Application.java
+++ stock-auto-trading/src/main/java/com/zhf/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -16,6 +17,7 @@ import java.time.LocalTime;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 @MapperScan("com.zhf.*.dao")
 @EnableConfigurationProperties({FebsProperties.class})
 @EnableCaching
